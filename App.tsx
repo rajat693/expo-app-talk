f66c196605d31ab9048369a50ff2922f199d8861
rajat693 <rajatchaudhary.rc693@gmail.com>
2025-01-12 22:08:02 +0530

    feat: updated alert example

--- App.tsx
+++ App.tsx
@@ -4,17 +4,23 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { Alert, AlertIcon, AlertText } from "@/components/ui/alert";
 import { InfoIcon } from "@/components/ui/icon";
 
+const AlertDemo = () => {
+  return (
+    <Alert>
+      <AlertIcon as={InfoIcon} />
+      <AlertText>Description of alert!</AlertText>
+    </Alert>
+  );
+};
+
 function NativeComponent() {
   return (
     <GluestackUIProvider mode="light">
       <View className="flex justify-center items-center h-screen">
-        <Alert>
-          <AlertIcon as={InfoIcon} />
-          <AlertText>Description of alert!</AlertText>
-        </Alert>
+        <AlertDemo />
       </View>
     </GluestackUIProvider>
   );
 }
-
+  
 export default NativeComponent;
